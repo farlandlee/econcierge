c265e29cb88841d82b4b4aac147f463c9390edad
Matthew Lee <matthew@farlandlee.com>
2015-12-08 11:37:10 -0700

    made forms on home page and activity page use actual activities and set the action to the activities page. built the activity search result page. modified app.js to automatically submit the form on the activty page when activity is changed changed form_for :name option to :as since I got a notice that name is deprecated suggested activities on the home page are now a list of all activities search forms now use get method to create sharable URLS removed UTF-8 enforcement from search forms to keep the urls as clean as possible
    closes #5, closes #9

--- web/router.ex
+++ web/router.ex
@@ -21,7 +21,7 @@ defmodule Grid.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/activities", PageController, :activities
+    get "/activity", PageController, :activity
   end
 
   scope "/admin", Grid.Admin do
