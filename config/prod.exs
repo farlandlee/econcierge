20dcbe6c3b8b2e754ed2e4059306e9aaa0361ef0
Jeremy Hopple <jeremy@jthopple.com>
2016-01-18 16:10:39 -0700

    HOTFIX: Updated prod host

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :grid, Grid.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "outpost-grid.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
