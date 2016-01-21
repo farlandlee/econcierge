c48caca5efbd5994ee47ffb79ace2310ac4dc015
Matt Enlow <matt@novaugust.net>
2016-01-21 13:17:03 -0700

    Add product price people count Closes #117

--- web/views/admin/vendor/product/price_view.ex
+++ web/views/admin/vendor/product/price_view.ex
@@ -1,3 +1,12 @@
 defmodule Grid.Admin.Vendor.Product.PriceView do
   use Grid.Web, :view
+
+  def format_form_errors(changeset) do
+    changeset
+    |> Ecto.Changeset.traverse_errors(fn
+      {msg, opts} -> String.replace(msg, "%{count}", to_string(opts[:count]))
+      msg -> msg
+    end)
+    |> Enum.map(fn {attr, message} -> {humanize(attr), message} end)
+  end
 end
