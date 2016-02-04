280706fccc27d60f851c3c829168588f91881431
Matt Enlow <matt@novaugust.net>
2016-02-03 19:19:12 -0700

    Add html5 history enabled ember app to frontend
    Closes #198

---
+++ web/controllers/search_controller.ex
@@ -0,0 +1,9 @@
+defmodule Grid.SearchController do
+  use Grid.Web, :controller
+
+  def index(conn, _) do
+    conn
+    |> put_resp_header("content-type", "text/html")
+    |> send_file(200, "priv/static/search/index.html")
+  end
+end
