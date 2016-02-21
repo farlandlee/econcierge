cbe4d5e144b3d1c0e06aaf8b72e8c25b9def2d22
Jeremy Hopple <jeremy@jthopple.com>
2016-02-21 15:16:49 -0700

    No Issue: Disallow same day, or past

--- client/app/components/grid-calendar.js
+++ client/app/components/grid-calendar.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import moment from 'moment';
+
+const {computed} = Ember;
 
 export default Ember.Component.extend({
+  minDate: moment().add(1, 'days'),
+
+  formattedMinDate: computed('minDate', {
+    get () {
+      return this.get('minDate').format('YYYY-MM-DD');
+    }
+  })
 });
