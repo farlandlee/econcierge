6f895f1760ad8025d33dee20196d76941483a96c
Matthew Lee <matthew@farlandlee.com>
2016-02-23 17:55:58 -0700

    Product Card Adjustments
    closes #447
    
    - Change button to "Click to Book"
    - Truncate title to one line: hovering title underlines and clicks to "Click To Book"
    - Truncate description to 4 lines
    	- I truncated this to 180 characters.  I also found this, https://www.npmjs.com/package/ember-truncate, that claims to do it by lines but couldn't get it to work.
    - Only show default price (with *message if there are more options)
    - Very light grey border on vendor icon.
    - Remove address and add to book modal (i.e. back of card)
    - Remove the "Trip Advisor Traveller Rating" for vendors that aren't in TripAdvisor
    - removed commented out css
    - added real truncate.js tests
    - changed tripadvisor to use URL to see if defined

---
+++ client/tests/unit/helpers/truncate-test.js
@@ -0,0 +1,37 @@
+import { truncate } from '../../../helpers/truncate';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | truncate');
+
+const longString = ["Buy FishCo's A superb Fly Fishing experience. Donec sodales sagittis magna. Nulla sit amet est. Cras non dolor. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam."];
+const shortString = ["Buy FishCo's A superb Fly Fishing experience. Donec sodales sagittis magna. Nulla sit amet est. Cras non dolor. Pellentesque libero tortor, tincidunt et, tincidunt eget"];
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = truncate(longString,{ limit: 180 });
+  assert.equal(result, "Buy FishCo's A superb Fly Fishing experience. Donec sodales sagittis magna. Nulla sit amet est. Cras non dolor. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper ...");
+});
+
+test('it works with null text', function(assert){
+  let result = truncate([],{ limit: 180 });
+  assert.equal(result, "");
+});
+
+test('it works with undefined text', function(assert){
+  let result = truncate([undefined],{ limit: 180 });
+  assert.equal(result, "");
+});
+
+test('it works with shorter string than limit', function(assert) {
+  let result = truncate(shortString,{ limit: 180 });
+  assert.equal(result, "Buy FishCo's A superb Fly Fishing experience. Donec sodales sagittis magna. Nulla sit amet est. Cras non dolor. Pellentesque libero tortor, tincidunt et, tincidunt eget");
+});
+
+test('it works with no limit', function(assert) {
+  let result = truncate(shortString);
+  assert.equal(result, "Buy FishCo's A superb Fly F...");
+});
+
+test('it works with no limit in defined namedArgs', function(assert){
+  let result = truncate(shortString,{ foo: 180 });
+  assert.equal(result, "Buy FishCo's A superb Fly F...");
+});
