6bdf815eb8e4875041f60f8faced4bb7609120bd
Matthew Lee <matthew@farlandlee.com>
2016-02-25 09:47:25 -0700

    Experience Styling and various fixes
    closes #485
    closes #486
    
    - The big image should always be displayed in the same size/ratio.  There are some that are smaller than others
    - The thumbs and view next experience controls should be bottom aligned
    - What to do when the description is long?
    - make product name bold on card
    - make product name space always fit 2 lines even when only one
    - put flaticons above descriptions for location & duration
    - change default text on placeholder images
    - fix full overlay bugs
    - Push "Click to Book" to bottom of product card
    - update placeholdit test to expect text
    - updated placehold-it.js to use template for string

--- client/app/helpers/placehold-it.js
+++ client/app/helpers/placehold-it.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function placeholdIt(params) {
   let [image, dimensions] = params;
-  return image ? image : 'http://placehold.it/' + dimensions;
+  return image ? image : `http://placehold.it/${dimensions}?text=Image+Coming+Soon`;
 }
 
 export default Ember.Helper.helper(placeholdIt);
