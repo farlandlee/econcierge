6df9b8a8bfdd2ad7afce824f66688aefe4717f65
Matt Enlow <matt@novaugust.net>
2016-02-24 19:07:18 -0700

    Show only default price on product card
    Closes #488
    - render default price by finding it out of prices
    - fix bug in product view test (and update it too)
    - fix bug in product controller test

--- client/app/models/product.js
+++ client/app/models/product.js
@@ -12,7 +12,7 @@ export default DS.Model.extend({
   name: DS.attr(),
   pickup: DS.attr('boolean'),
   duration: DS.attr('number'),
-  defaultPrice: DS.attr('number'),
+  defaultPrice: DS.attr(),
 
   vendor: DS.belongsTo('vendor'),
   experience: DS.belongsTo('experience'),
