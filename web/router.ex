005f530ec47104b5b7712d88afa9ecb2d8fa3541
Jeremy Hopple <jeremy@jthopple.com>
2016-02-27 10:27:31 -0700

    Force SSL
    Ref #427
    
    - Redirect to SSL for the browser pipeline for production only

--- web/router.ex
+++ web/router.ex
@@ -4,6 +4,10 @@ defmodule Grid.Router do
   alias Grid.Plugs
 
   pipeline :browser do
+    if Mix.env() == :prod do
+      plug Plug.SSL, rewrite_on: [:x_forwarded_proto]
+    end
+
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
