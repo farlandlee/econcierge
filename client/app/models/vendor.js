0d494a4896f1c1ff235122ee8908e880996e8860
Matt Enlow <matt@novaugust.net>
2016-05-10 13:02:32 -0600

    Add vendor icons
    Closes #955

--- client/app/models/vendor.js
+++ client/app/models/vendor.js
@@ -1,26 +1,16 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
-const {computed} = Ember;
+const {
+  computed: {alias}
+} = Ember;
 
 export default DS.Model.extend({
   name: DS.attr(),
   description: DS.attr(),
   slug: DS.attr(),
   cancellationPolicyDays: DS.attr('number'),
-  tripadvisorUrl: DS.attr(),
-  tripadvisorReviewUrl: DS.attr(),
   tripadvisorRating: DS.attr(),
-  tripadvisorRatingImageUrl: DS.attr(),
-  tripadvisorReviewsCount: DS.attr(),
   defaultImage: DS.attr(),
-
-  hasTripadvisor: computed('tripadvisorUrl','tripadvisorReviewUrl', {
-    get() {
-      let url = this.get('tripadvisorUrl');
-      let reviewUrl = this.get('tripadvisorReviewUrl');
-
-      return url && reviewUrl;
-    }
-  })
+  image: alias('defaultImage')
 });
