6c0f6df21c071b0de8606fe0b004ac24c9aa3238
Matt Enlow <matt@novaugust.net>
2016-05-13 11:37:53 -0600

    The best product loading transition ever
    Refer #1061
    - for model swaps within the explore route, just dim opacity on cards
    - for route entry, still show the page-wide loading spinner

--- client/app/routes/activity/explore.js
+++ client/app/routes/activity/explore.js
@@ -7,14 +7,6 @@ import {parseDate} from 'client/utils/time';
 const {$, RSVP} = Ember;
 
 export default Ember.Route.extend(NotFoundMixin, ResetScrollMixin, RouteTitleMixin, {
-  activate () {
-    $('body').addClass('no-footer');
-  },
-
-  deactivate () {
-    $('body').removeClass('no-footer');
-  },
-
   model ({category_slug, date}) {
     if (date && !parseDate(date).isValid()) {
       return this.replaceWith({queryParams: {date: undefined}});
@@ -61,5 +53,35 @@ export default Ember.Route.extend(NotFoundMixin, ResetScrollMixin, RouteTitleMix
     controller.set('products', products);
     controller.set('activity', this.modelFor('activity'));
     controller.set('activities', this.store.peekAll('activity'));
+  },
+
+  activate () {
+    // manually track whether transitions into this route are from other routes or from interior,
+    // so that we can display the correct loading page
+    this.set('entering', true);
+    $('body').addClass('no-footer');
+  },
+
+  deactivate () {
+    $('body').removeClass('no-footer');
+  },
+
+  actions: {
+    didTransition () {
+      if (this.get('entering')) {
+        this.set('entering', false);
+      }
+    },
+
+    loading (transition) {
+      // if this is a model swap within the route, do custom loading
+      if (!this.get('entering') && this.controller) {
+        this.controller.set('loading', true);
+        transition.finally(() => this.controller.set('loading', false));
+        return false;
+      }
+      // bubble up for a normal loading substate if we aren't doing our custom load behaviour
+      return true;
+    }
   }
 });
