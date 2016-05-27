545575215b961d66c0050008872bd7ca12618edc
Jeremy Hopple <jeremy@jthopple.com>
2016-05-27 09:09:23 -0600

    HOTFIX: Remove unused function call

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,12 +21,10 @@
 import fulloverlaySelect from "./fulloverlay-select";
 import howItWorks from "./how-it-works";
 import kioskSlideshow from "./kiosk-slideshow";
-import "./track-slide-click";
 
 jQuery(function($){
   $(document).foundation();
   howItWorks($);
   fulloverlaySelect($);
   kioskSlideshow($);
-  trackSlideClick($);
 });
